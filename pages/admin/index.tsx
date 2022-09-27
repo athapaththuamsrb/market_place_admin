af51c616eb0f5a5630d2c36285c85818691ecc5e
Sanduni-Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2022-09-27 13:30:05 +0530

    admin dashboard and reported user table complete

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -16,106 +16,125 @@ import {
   GridToolbarContainer,
   GridToolbarExport,
 } from "@mui/x-data-grid";
-import { Box, Button } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography } from "@mui/material";
+
+import BlockIcon from '@mui/icons-material/Block';
 import VerifiedIcon from "@mui/icons-material/Verified";
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import Tooltip from "@mui/material/Tooltip";
-import GroupIcon from "@mui/icons-material/Group";
-import { Container } from "@mui/system";
-
-const columns = [
-  { field: "id", headerName: "ID", width: 50, align: "center" },
-  { field: "User_ID", headerName: "User_ID", type: "string", width: 250 },
-  { field: "Name", headerName: "Name", type: "string", width: 150 },
-  { field: "Date", headerName: "Joined Date", type: "Date", width: 100 },
-  {
-    field: "Total",
-    headerName: "Total NFTs",
-    type: "Number",
-    width: 100,
-    align: "center",
-  },
-  {
-    field: "Created",
-    headerName: "Created NFTs",
-    type: "Number",
-    width: 100,
-    align: "center",
-  },
-  {
-    field: "Volume",
-    headerName: "Total volume",
-    type: "Number",
-    width: 150,
-    align: "center",
-  },
-  {
-    field: "actions",
-    type: "actions",
-    width: 100,
-    headerName: "Review",
-    getActions: (params: GridRowParams) => [
-      <Tooltip title="Delete admin">
-        <GridActionsCellItem icon={<DeleteIcon />} label="Delete" />
-      </Tooltip>,
-      <Tooltip title="Verify admin">
-        <GridActionsCellItem icon={<VerifiedIcon />} label="Verify" />
-      </Tooltip>, //onaction to be put
-      //onClick={toggleAdmin(params.id)}
-    ],
-  },
-];
-
-const onRowsSelectionHandler = (ids) => {
-  const selectedRowsData = ids.map((id) => rows.find((row) => row.id === id));
-  console.log(selectedRowsData);
-};
-
-function CustomToolbar() {
-  return (
-    <GridToolbarContainer>
-      <GridToolbarExport />
-    </GridToolbarContainer>
-  );
-}
+import Link from "next/link";
 
 const reportedUsers: NextPage = () => {
-  const [rows, setRows] = useState([]);
+  const columns = [
+    { field: "id", headerName: "ID", width: 50, align: "center" },
+    { field: "User_ID", headerName: "User_ID", type: "string", width: 300},
+    { field: "Name", headerName: "Name", type: "string", width: 250 },
+    { field: "Date", headerName: "Joined Date", type: "Date", width: 150 },
+    {
+      field: "Total",
+      headerName: "Total NFTs",
+      type: "Number",
+      width: 150,
+      align: "center",
+    },
+    {
+      field: "Created",
+      headerName: "Created NFTs",
+      type: "Number",
+      width: 150,
+      align: "center",
+    },
+    {
+      field: "Volume",
+      headerName: "Total volume",
+      type: "Number",
+      width: 150,
+      align: "center",
+    },
+    {
+      field: "Status",
+      headerName: "Status",
+      type: "string",
+      width: 150,
+      align: "center",
+    },
+    {
+      field: "actions",
+      type: "actions",
+      width: 200,
+      headerName: "View",
+      getActions: (params: GridRowParams) => [
+        <Button variant="solid" color="primary">
+          <Link href={`/admin/users/${params.row.id}`}>
+            <a>View Account</a>
+          </Link>
+        </Button>
+      ],
+    },
+  ];  
 
+  const [rows, setRows] = useState([]);
+  const [id,setId] = useState('');
+  
   useEffect(() => {
     fetch("/api/rows")
       .then((res) => res.json())
       .then((data) => setRows(data));
-  });
+  },[]);  
 
+   
+  const onRowsSelectionHandler = (ids) => {
+    const selectedRowsData = ids.map((id) => rows.find((row) => row.id === id));
+    console.log(selectedRowsData);
+  };
+  const handleRowClick = (params) => {
+    console.log(params.row.id);
+    
+  };
+
+  const directPage = (ids) => { 
+    return(
+      <Link href="/admin/report/user"></Link>
+    )
+  };
+  
+  function CustomToolbar() {
+    return (
+      <GridToolbarContainer>
+        <GridToolbarExport />
+      </GridToolbarContainer>
+    );
+  }
   return (
     <div>
       <Box
         sx={{
           flexGrow: 1,
-          width: "72%",
+          width: "85%",
           marginX: "auto",
           marginTop: "100px",
           marginBottom: "100px",
           height: 750,
           backgroundColor: "white",
-          borderRadius: "10px",
+          borderRadius: "5px",
+          
         }}
       >
         <DataGrid
-          sx={{ m: 2 }}
+          sx={{ m: 0 }}
           rows={rows}
           columns={columns}
-          pageSize={12}
+          pageSize={11}
           rowsPerPageOptions={[5]}
-          onSelectionModelChange={(id) => onRowsSelectionHandler(id)}
+          onSelectionModelChange={(id) => setId(id)}
+          onRowClick={(id) =>  handleRowClick(id)}
           components={{
             Toolbar: CustomToolbar,
           }}
         />
       </Box>
     </div>
+    
   );
 };
 
