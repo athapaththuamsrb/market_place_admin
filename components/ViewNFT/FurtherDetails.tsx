e2ca2e5477e7bc6e656908b4e628bf7741bcb2bf
Kumuthu <kumuthua@gmail.com>
2022-11-02 19:31:13 +0530

    Further details updated

--- components/ViewNFT/FurtherDetails.tsx
+++ components/ViewNFT/FurtherDetails.tsx
@@ -5,8 +5,10 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import InfoIcon from "@mui/icons-material/Info";
+import CardContent from "@mui/material/CardContent";
 
 import { FC, useState } from "react";
+import Card from "@mui/material/Card";
 
 interface FurtherDetailsProps {
   tokenID: number;
@@ -32,7 +34,125 @@ const FurtherDetails: FC<FurtherDetailsProps> = (props) => {
           <Typography>Further Details</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography
+          <Card
+            sx={{
+              boxShadow: 0,
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "10px",
+              }}
+            >
+              <Typography variant="h4">Token ID :</Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ fontWeight: 600, fontSize: "15px" }}
+              >
+                {props.tokenID}
+              </Typography>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "10px",
+              }}
+            >
+              <Typography variant="h4">Creator :</Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ fontWeight: 600, fontSize: "13px" }}
+              >
+                {props.creator}
+              </Typography>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "10px",
+              }}
+            >
+              <Typography variant="h4">Token Standard :</Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ fontWeight: 600, fontSize: "15px" }}
+              >
+                ERC721
+              </Typography>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "10px",
+              }}
+            >
+              <Typography variant="h4">Collection :</Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ fontWeight: 600, fontSize: "13px" }}
+              >
+                {props.collection}
+              </Typography>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "10px",
+              }}
+            >
+              <Typography variant="h4">Blockchain :</Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ fontWeight: 600, fontSize: "15px" }}
+              >
+                Ethereum
+              </Typography>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "10px",
+              }}
+            >
+              <Typography variant="h4">Metadata :</Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ fontWeight: 600, fontSize: "15px" }}
+              >
+                Centralized
+              </Typography>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "10px",
+              }}
+            >
+              <Typography variant="h4">Creator Fee :</Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ fontWeight: 600, fontSize: "15px" }}
+              >
+                0%
+              </Typography>
+            </div>
+          </Card>
+          {/* <Typography
             sx={{
               fontWeight: 400,
               fontSize: 15,
@@ -108,7 +228,7 @@ const FurtherDetails: FC<FurtherDetailsProps> = (props) => {
             align="left"
           >
             Creator Fee : 0%
-          </Typography>
+          </Typography> */}
         </AccordionDetails>
       </Accordion>
     </div>
